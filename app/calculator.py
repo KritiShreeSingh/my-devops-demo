c31e9d7ea551bc488de872db65a02a333722781b
KritiShreeSingh <kritisingh2059@gmail.com>
2026-07-09 07:51:28 +0545

    Add apply_discount function to calculate discounts

---
+++ app/calculator.py
@@ -0,0 +1,9 @@
+def apply_discount(amount, discount_percent):
+    if amount <= 0:
+        raise ValueError("amount must be > 0")
+
+    if discount_percent < 0 or discount_percent > 80:
+        raise ValueError("discount must be between 0 and 80")
+
+    discounted = amount * (1 - discount_percent / 100)
+    return round(discounted, 2)
