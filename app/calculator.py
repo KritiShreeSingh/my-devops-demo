54cdac47394c7019d17f89c8ae20080c180989ad
KritiShreeSingh <kritisingh2059@gmail.com>
2026-07-15 06:42:29 +0545

    Add validation for discount percentage in apply_discount

--- app/calculator.py
+++ app/calculator.py
@@ -2,6 +2,8 @@ def apply_discount(amount, discount_percent):
     if amount <= 0:
         raise ValueError("amount must be > 0")
 
+
+    
     if discount_percent < 0 or discount_percent > 80:
         raise ValueError("discount must be between 0 and 80")
 
